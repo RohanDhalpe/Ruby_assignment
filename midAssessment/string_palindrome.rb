36751f887b276b10c057ea7f771bd0b9b82388f8
Rohan Dhalpe <rohandhalpe05@gmail.com>
2024-01-23 19:08:45 +0530

    Mid assessment updated

--- midAssessment/string_palindrome.rb
+++ midAssessment/string_palindrome.rb
@@ -13,8 +13,5 @@ puts 'Enter a string to check for palindrome:'
 user_input = gets.chomp
 
 # Check if the entered string is a palindrome or not
-if palindrome?(user_input)
-  puts 'The entered string is a palindrome.'
-else
-  puts 'The entered string is not a palindrome.'
-end
+puts palindrome?(user_input) ? 'The entered string is a palindrome.' : 'The entered string is not a palindrome.'
+
